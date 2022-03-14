278957f999e60d44a625b633a488871401d00304
NellaiRoshan <nellainayagamsss@gmail.com>
2022-03-14 12:17:25 +0530

    nellai new class

---
+++ src/test/java/Test.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+	    WebDriverManager.chromedriver().setup();
+	    WebDriver driver = new ChromeDriver();
+	    driver.get("https://www.facebook.com/");
+	    
+	   
+	    
+	
+		
+		
+		
+	}
+
+}
